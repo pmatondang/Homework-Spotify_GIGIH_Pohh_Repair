5fb89b811e80636293b151b594248ec48cf4f4e8
Poibe Leny Naomi <poibematondang30@gmail.com>
2022-04-13 22:01:36 +0700

    create repair all homework

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import "./index.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import CreatePlayList from "./page/createPlaylist";
+import Login from "./page/login";
+import { useSelector } from "react-redux";
+import Navbar from "./Component/Navbar";
+
+function App() {
+  const isLogin = useSelector((state) => state.auth.isLogin);
+
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <Navbar />
+          <Switch>
+            <Route path={"/create-playlist"}>
+              {isLogin ? <CreatePlayList /> : <Redirect to={"/"} />}
+            </Route>
+            <Route path={"/"}>
+              <Login />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
